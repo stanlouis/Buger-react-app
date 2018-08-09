ee951d33c3531c3b86236cc2d9badcfa09664837
Stanley Louis <stanlouis@msn.com>
2018-08-09 15:18:31 -0400

    Add BurgerBuilder component skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './components/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Layout>Hello</Layout>
+        <Layout>
+          <BurgerBuilder/>
+        </Layout>
       </div>
     );
   }
